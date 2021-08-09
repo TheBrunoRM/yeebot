bfb0b4cb692e7aa0439d2dc94e5586828101de56
TheBrunoRM <bruno12345best@gmail.com>
2021-08-09 01:20:48 -0300

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+var myColors = ["#00ff0d", "#00c90a", "#009107", "#006105", "#003d03"];
+
+let element = document.getElementsByTagName("h1")[0]
+
+let text = element.innerHTML;
+
+let offset = 0;
+
+function update() {
+    let newCont = '';
+
+    for (var i = 0; i < text.split("").length; i++) {
+        let color = myColors[(i - offset) % myColors.length];
+        newCont += `<span style="color:${color}">` + text.charAt(i) + '</span>';
+    }
+
+    element.innerHTML = newCont;
+    offset++;
+}
+
+var speed = 100;
+setInterval(update, speed);
